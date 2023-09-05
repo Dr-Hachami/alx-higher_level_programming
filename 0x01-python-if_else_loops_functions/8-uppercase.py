0b30353f66a186d45ad8418e28247d9a4c81b88b
Yassine hachami <dr.y.hachmi@gmail.com>
2023-09-05 09:07:13 -0400

    Updating the file 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Author --Yassine Hachami
 
-def uppercase(str):"
+def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
