d172b3790d7c274f967a824f922cb3197d55d78d
Yassine hachami <dr.y.hachmi@gmail.com>
2023-09-13 09:33:39 -0400

    Uploading the files

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author --Yassine Hachami
+
+def common_elements(set_1, set_2):
+    return (set_1 & set_2)
---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author --Yassine Hachami
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return (a_dictionary)
